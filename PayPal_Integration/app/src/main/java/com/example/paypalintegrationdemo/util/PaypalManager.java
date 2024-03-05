79e7633b566469776553a53d48990beadea4c8e3
Usmi Mukherjee <35424412+usmimukherjee@users.noreply.github.com>
2024-03-05 16:06:19 -0400

    Merge remote changes

---
+++ PayPal_Integration/app/src/main/java/com/example/paypalintegrationdemo/util/PaypalManager.java
@@ -0,0 +1,9 @@
+package com.example.paypalintegrationdemo.util;
+import com.example.paypalintegrationdemo.BuildConfig;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+public class PaypalManager {
+
+
+
+
+}
---
+++ PayPal_Integration/app/src/main/java/com/example/paypalintegrationdemo/views/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.paypalintegrationdemo.views;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.math.BigDecimal;
+
+import com.example.paypalintegrationdemo.BuildConfig;
+import com.example.paypalintegrationdemo.R;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
+
+import com.example.paypalintegrationdemo.util.PaypalManager;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = MainActivity.class.getName();
+
+    //launching a previously-prepared call to start the process of executing an ActivityResultContract.
+    private ActivityResultLauncher<Intent> activityResultLauncher;
+    private PayPalConfiguration payPalConfig;
+
+    //UI Elements
+    private EditText enterAmtET;
+    private Button payNowBtn;
+    private TextView paymentStatusTV;
+
+//    private static final String PAYPAL_CLIENT_ID = "AXueFBuceTIerT_9lpLQzoYDNj6NcHfBy1RlDqYaoF04KMKoI-tTmi65mkRETaUE8IWXq81YKupPCMQA";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        configPayPal();
+        initActivityLauncher();
+        setListeners();
+    }
+
+    private void init() {
+        //initializing ui elements
+        enterAmtET = findViewById(R.id.enterAmtET);
+        payNowBtn = findViewById(R.id.payNowBtn);
+        paymentStatusTV = findViewById(R.id.paymentStatusTV);
+    }
+
+    private void configPayPal() {
+        //configuring paypal i.e defining we're using SANDBOX Environment and setting the paypal client id
+        payPalConfig = new PayPalConfiguration()
+                .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
+                .clientId(BuildConfig.PAYPAL_CLIENT_ID);
+    }
+
+    private void initActivityLauncher() {
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK) {
+                        final PaymentConfirmation confirmation = result.getData().getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+                        if (confirmation != null) {
+                            try {
+                                // Get the payment details
+                                String paymentDetails = confirmation.toJSONObject().toString(4);
+                                Log.i(TAG, paymentDetails);
+                                // Extract json response and display it in a text view.
+                                JSONObject payObj = new JSONObject(paymentDetails);
+                                String payID = payObj.getJSONObject("response").getString("id");
+                                String state = payObj.getJSONObject("response").getString("state");
+                                paymentStatusTV.setText(String.format("Payment %s%n with payment id is %s", state, payID));
+                            } catch (JSONException e) {
+                                Log.e("Error", "an extremely unlikely failure occurred: ", e);
+                            }
+                        }
+                    } else if (result.getResultCode() == PaymentActivity.RESULT_EXTRAS_INVALID) {
+                        Log.d(TAG, "Launcher Result Invalid");
+                    } else if (result.getResultCode() == Activity.RESULT_CANCELED) {
+                        Log.d(TAG, "Launcher Result Cancelled");
+                    }
+                });
+    }
+
+    private void setListeners() {
+        payNowBtn.setOnClickListener(v -> processPayment());
+    }
+
+    private void processPayment() {
+        final String amount = enterAmtET.getText().toString();
+        final PayPalPayment payPalPayment = new PayPalPayment(new BigDecimal(
+                amount), "CAD", "Purchase Goods", PayPalPayment.PAYMENT_INTENT_SALE);
+
+        // Create Paypal Payment activity intent
+        final Intent intent = new Intent(this, PaymentActivity.class);
+        // Adding paypal configuration to the intent
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, payPalConfig);
+        // Adding paypal payment to the intent
+        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, payPalPayment);
+        // Starting Activity Request launcher
+        activityResultLauncher.launch(intent);
+    }
+
+}
